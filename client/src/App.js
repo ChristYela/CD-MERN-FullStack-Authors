55e90b4025bba9119d081d879781e25d6ff6f06b
Cristian Andres Yela <50997813+ChristYela@users.noreply.github.com>
2022-11-29 11:41:38 -0500

    Update App.js (corrected edition with credentials)

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActualizarAutor from './componentes/ActualizarAutor';
-import NuevoAutor from './componentes/NuevoAutor';
-import TodosAutores from './componentes/TodosAutores';
 import Error from './componentes/Error';
 import LoginRegistro from './componentes/LoginRegistro';
+import NuevoAutor from './componentes/NuevoAutor';
+import TodosAutores from './componentes/TodosAutores';
 
 const App = () => {
   return(
     <div className="container">
       <BrowserRouter>
         <Switch>
-          <Route path="/login" exact render={()=><LoginRegistro/>}/>
-          <Route path="/" exact render={() => <TodosAutores />} />
+          <Route path="/login" render={()=> <LoginRegistro/> } />
+          <Route path="/" exact render={()=><TodosAutores />} />
           <Route path="/nuevo" render={() => <NuevoAutor />} />
-          <Route path="/autor/editar/:id" render={() => <ActualizarAutor/>} />
-          <Route path="/error" render={() => <Error />}/>
+          <Route path="/autor/editar/:id" render={(pathParams) => <ActualizarAutor {...pathParams} />}/>
+          <Route path="/error" render={() => <Error /> } />
           <Route path="*" render={() => <Error /> } />
         </Switch>
       </BrowserRouter>
     </div>
   )
-
 }
-
 
 export default App;
